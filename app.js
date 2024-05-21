5a7177b0ce3cd2355f74910fdec7a221ec51ae65
Vladislav Perdoch <v.perdoch,st@spseiostrava.cz>
2024-05-21 12:06:23 +0200

    HTML,CSS,JS

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 console.log("hello");
+
+
+
+function walkTheDog(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            const walkDog = false;
+
+            if(walkDog){
+             resolve("Walked the dog");
+            }else{
+             reject("Didnt walk the dog");
+            }
+
+
+        }, 1500);
+    });
+}
+    
+
+async function doChores(){
+
+
+    try{
+        const walkDogResult = await walkTheDog();
+        console.log(walkDogResult);
+
+    }catch(error){
+        console.error(error);
+    }
+    
+}
+
+
+doChores()
